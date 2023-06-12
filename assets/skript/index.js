e186289180847db49a1e16ff58b69217e9348159
1100-git <vlad.ribnik@gmail.com>
2023-06-12 17:23:25 +0300

    working

---
+++ assets/skript/index.js
@@ -0,0 +1,33 @@
+// burger
+const bool = false;
+const body = document.querySelector("body");
+const burger = document.querySelector(".header-btn");
+const menu = document.querySelector(".nav");
+
+
+burger.addEventListener('click',
+  function () {
+    burger.classList.toggle("header-btn-active");
+    menu.classList.toggle("nav-active");
+    document.body.classList.toggle("stop-scroll");
+  }
+);
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click',
+    function () {
+      burger.classList.remove("header-btn-active");
+      menu.classList.remove("nav-active");
+      document.body.classList.remove("stop-scroll");
+    })
+});
+
+body.addEventListener('click',
+  function (el) {
+    if (el.target !== burger && el.target !== menu) {
+      burger.classList.remove("header-btn-active");
+      menu.classList.remove("nav-active");
+      document.body.classList.remove("stop-scroll");
+    }
+  }
+);
